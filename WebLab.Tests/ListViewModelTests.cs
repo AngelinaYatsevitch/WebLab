83152253aa8d2a6e7605e1362d6f73ca7c9c10f7
AngelinaYatsevitch <3722600@gmail.com>
2023-10-12 14:00:35 +0300

    Lab6

---
+++ WebLab.Tests/ListViewModelTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using WebLab.Entities;
+using WebLab.Models;
+using WebLab.Tests;
+using Xunit;
+namespace WebLab.Tests
+{
+    public class ListViewModelTests
+    {
+        [Fact]
+        public void ListViewModelCountsPages()
+        {
+            // Act
+            var model = ListViewModel<Dish>
+            .GetModel(TestData.GetDishesList(), 1, 3);
+            // Assert
+            Assert.Equals(2, model.TotalPages);
+        }
+        [NUnit.Framework.Theory]
+        [MemberData(memberName: nameof(TestData.Params),
+        MemberType = typeof(TestData))]
+        
+        
+        public void ListViewModelSelectsCorrectQty(int page, int qty, int id)
+        {
+            // Act
+            var model = ListViewModel<Dish>
+            .GetModel(TestData.GetDishesList(), page, 3);
+            // Assert
+            Assert.Equals(qty, model.Count);
+        }
+        [NUnit.Framework.Theory]
+        [MemberData(memberName: nameof(TestData.Params),
+        MemberType = typeof(TestData))]
+        
+        
+        public void ListViewModelHasCorrectData(int page, int qty, int id)
+        {
+            // Act
+            var model = ListViewModel<Dish>
+            .GetModel(TestData.GetDishesList(), page, 3);
+            // Assert
+            Assert.Equals(id, model[0].DishId);
+        }
+    }
+}
--- WebLab/Controllers/ProductController.cs
+++ WebLab/Controllers/ProductController.cs
@@ -4,25 +4,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebLab.Entities;
+using WebLab.Models;
 
 namespace WebLab.Controllers
 {
     public class ProductController : Controller
     {
-        List<Dish> _dishes;
+        public List<Dish> _dishes;
         List<DishGroup> _dishGroups;
+        int _pageSize;
         public ProductController()
         {
+            _pageSize = 3;
             SetupData();
         }
-        public IActionResult Index()
+        public IActionResult Index(int? group, int pageNo = 1)
         {
-            return View(_dishes);
+            var dishesFiltered = _dishes
+            .Where(d => !group.HasValue || d.DishGroupId == group.Value);
+            // Поместить список групп во ViewData
+            ViewData["Groups"] = _dishGroups;
+            // Получить id текущей группы и поместить в TempData
+            ViewData["CurrentGroup"] = group ?? 0;
+            //var items = _dishes
+            //.Skip((pageNo - 1) * _pageSize)
+            //.Take(_pageSize)
+            //.ToList();
+            //return View(items);
+            return View(ListViewModel<Dish>.GetModel(dishesFiltered,
+            pageNo, _pageSize));
         }
-        /// <summary>
-        /// Инициализация списков
-        /// </summary>
-        private void SetupData()
+            /// <summary>
+            /// Инициализация списков
+            /// </summary>
+            private void SetupData()
         {
             _dishGroups = new List<DishGroup>
             {
