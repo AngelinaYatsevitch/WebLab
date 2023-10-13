5dd9de98d223bd0014bf1462d1ebbd491b205352
AngelinaYatsevitch <3722600@gmail.com>
2023-10-13 09:41:35 +0300

    Lab8

--- WebLab.Tests/ProductControllerTests.cs
+++ WebLab.Tests/ProductControllerTests.cs
@@ -7,15 +7,26 @@ using NUnit.Framework;
 using Xunit;
 using Moq;
 using Microsoft.AspNetCore.Http;
+using WebLab.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebLab.Tests
 {
-
-    public class TestData
+    public class ProductControllerTests
     {
-
-        public static List<Dish> GetDishesList()
+        DbContextOptions<ApplicationDbContext> _options;
+        public ProductControllerTests()
         {
+            _options =
+            new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "testDb")
+            .Options;
+        }
+        [NUnit.Framework.Theory]
+        [MemberData(nameof(TestData.Params), MemberType = typeof(TestData))]
+        public void ControllerGetsProperPage(int page, int qty, int id)
+        {
+            // Arrange
             // Контекст контроллера
             var controllerContext = new ControllerContext();
             // Макет HttpContext
@@ -23,65 +34,61 @@ namespace WebLab.Tests
             moqHttpContext.Setup(c => c.Request.Headers)
             .Returns(new HeaderDictionary());
             controllerContext.HttpContext = moqHttpContext.Object;
-            var controller = new ProductController()
-            { ControllerContext = controllerContext };
-            return new List<Dish>
+            //заполнить DB данными
+            using (var context = new ApplicationDbContext(_options))
             {
-            new Dish{ DishId=1, DishGroupId=1},
-            new Dish{ DishId=2, DishGroupId=1},
-            new Dish{ DishId=3, DishGroupId=2},
-            new Dish{ DishId=4, DishGroupId=2},
-            new Dish{ DishId=5, DishGroupId=3}
-            };
+                TestData.FillContext(context);
+            }
+            using (var context = new ApplicationDbContext(_options))
+            {
+                // создать объект класса контроллера
+                var controller = new ProductController(context)
+                { ControllerContext = controllerContext };
+                // Act
+                var result = controller.Index(group: null, pageNo: page) as ViewResult;
+                var model = result?.Model as List<Dish>;
+                // Assert
+                Assert.NotNull(model);
+                Assert.Equal(qty, model.Count);
+                Assert.Equal(id, model[0].DishId);
+            }
+            // удалить базу данных из памяти
+            using (var context = new ApplicationDbContext(_options))
+            {
+                context.Database.EnsureDeleted();
+            }
         }
-        public static IEnumerable<object[]> Params()
-        {
-            // 1-я страница, кол. объектов 3, id первого объекта 1
-            yield return new object[] { 1, 3, 1 };
-            // 2-я страница, кол. объектов 2, id первого объекта 4
-            yield return new object[] { 2, 2, 4 };
-        }
-    }
-
-    class ProductControllerTests
-    {
-        [NUnit.Framework.Theory]
-        [MemberData(nameof(TestData.Params), MemberType = typeof(TestData))]
-        
-        
-        public void ControllerGetsProperPage(int page, int qty, int id)
-        {
-            // Arrange
-            var controller = new ProductController();
-            controller._dishes = TestData.GetDishesList();
-            // Act
-            var result = controller.Index(pageNo: page, group: null) as ViewResult;
-            var model = result?.Model as List<Dish>;
-            // Assert
-            Assert.NotNull(model);
-            Assert.Equals(qty, model.Count);
-            Assert.Equals(id, model[0].DishId);
-        }
-
         [Fact]
         public void ControllerSelectsGroup()
         {
             // arrange
-            var controller = new ProductController();
-            var data = TestData.GetDishesList();
-            controller._dishes = data;
-            var comparer = Comparer<Dish>
-            .GetComparer((d1, d2) => d1.DishId.Equals(d2.DishId));
-            // act
-            var result = controller.Index(2) as ViewResult;
-            var model = result.Model as List<Dish>;
-            // assert
-            Assert.Equals(2, model.Count);
-            Assert.Equals(data[2], model[0], comparer);
+            // Контекст контроллера
+            var controllerContext = new ControllerContext();
+            // Макет HttpContext
+            var moqHttpContext = new Mock<HttpContext>();
+            moqHttpContext.Setup(c => c.Request.Headers)
+            .Returns(new HeaderDictionary());
+            controllerContext.HttpContext = moqHttpContext.Object;
+            //заполнить DB данными
+            using (var context = new ApplicationDbContext(_options))
+            {
+                TestData.FillContext(context);
+            }
+            using (var context = new ApplicationDbContext(_options))
+            {
+                var controller = new ProductController(context)
+                { ControllerContext = controllerContext };
+                var comparer = Comparer<Dish>.GetComparer((d1, d2) => d1.DishId.Equals(d2.DishId));
+                // act
+                var result = controller.Index(2) as ViewResult;
+                var model = result.Model as List<Dish>;
+                // assert
+                Assert.Equal(2, model.Count);
+                Assert.Equal(context.Dishes
+                .ToArrayAsync()
+                .GetAwaiter()
+                .GetResult()[2], model[0], comparer);
+            }
         }
-
-
-
     }
 }
-
