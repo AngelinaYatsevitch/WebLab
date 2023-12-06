de067e6acf364a8eea8714a783a69eccf711385f
AngelinaYatsevitch <3722600@gmail.com>
2023-12-06 10:51:39 +0300

    Lab8

--- WebLab/Data/ApplicationDbContext.cs
+++ WebLab/Data/ApplicationDbContext.cs
@@ -1,14 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using WebLab.Entities;
 
 namespace WebLab.Data
 {
-   
-    
+
+
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Dish> Dishes { get; set; }
--- WebLab/Services/DbInitializer.cs
+++ WebLab/Services/DbInitializer.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using System;
+﻿using Microsoft.AspNetCore.Identity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
